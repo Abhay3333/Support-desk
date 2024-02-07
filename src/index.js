a7b6e3579c8bab3d3dfd8aad9e50d0f53e09f8f2
Abhay Hemant Upadhaya <abhayupadhaya829@gmail.com>
2024-02-07 20:46:46 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { store } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 
@@ -23,7 +22,4 @@ root.render(
   </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
